07944ffdf29dc2c6a5e5da493336396023752805
marcos514 <marmarreyer@gmail.com>
2019-05-27 19:43:44 -0300

    Auth service + Camera pluging + log in

--- CodeForFood/src/app/interfaces/usuario.ts
+++ CodeForFood/src/app/interfaces/usuario.ts
@@ -1,11 +1,37 @@
 export interface Cliente {
+    uid:string;
+    foto:string;
+    nombre:string;
+    apellido:string;
+    mail:string;
+    dni:string;
+    genero:string;
+    nacimiento:string;
+    nacionalidad:string;
+    activo:boolean;
+    perfil:string;
 }
 
 export interface Empleado {
+    uid:string;
+    foto:string;
+    nombre:string;
+    apellido:string;
+    mail:string;
+    dni:string;
+    genero:string;
+    nacimiento:string;
+    nacionalidad:string;
+    cuil:string;
+    activo:boolean;
+    perfil:string;
 }
 
 export interface Anonimo {
-}
-
-export interface Usuario {
+    uid:string;
+    foto:string;
+    nombre:string;
+    mail:string;
+    activo:boolean;
+    perfil:string;
 }
