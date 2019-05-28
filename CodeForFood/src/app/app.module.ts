5708a78482b840885fc16a4ab46a733f49cd22ec
SkieeL <Skiel_Mafu-cs@hotmail.com>
2019-05-28 14:21:29 -0300

    Se agrega la pagina de carga de Mesa, se utilizan los plugins Camera y BarcodeScanner

--- CodeForFood/src/app/app.module.ts
+++ CodeForFood/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { Camera } from '@ionic-native/camera/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,8 +14,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { firebaseConfig } from "../environments/environment";
 
 import { AngularFireModule } from '@angular/fire';
-import { AngularFireAuthModule } from '@angular/fire/auth'
-import { AngularFirestoreModule, FirestoreSettingsToken } from '@angular/fire/firestore'
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule, FirestoreSettingsToken } from '@angular/fire/firestore';
 // import { Camera } from '@ionic-native/camera/ngx';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 
@@ -32,6 +34,8 @@ import { AngularFireStorageModule } from '@angular/fire/storage';
   providers: [
     StatusBar,
     SplashScreen,
+    BarcodeScanner,
+    Camera,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     { provide: FirestoreSettingsToken, useValue:{}}
   ],
--- CodeForFood/src/app/servicios/mesas.service.ts
+++ CodeForFood/src/app/servicios/mesas.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage } from '@angular/fire/storage';
+
+export interface objetoMesa {
+  numero: number;
+  espacios: number;
+  tipo: string;
+  qr: string;
+  foto: string;
+  ocupada: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class MesasService {
+  constructor(private db: AngularFirestore, private fireStorage: AngularFireStorage) { }
 
-  constructor() { }
+  addMesa(mesa: objetoMesa) {
+    this.db.collection('mesas').add(mesa).then(() => {
+      alert('Mesa cargada exitosamente!');
+    }).catch(error => {
+      alert(error);
+    });
+  }
+
+  async uploadFotoToFirebase(dataFoto: string) {
+    const picName = (new Date()).getTime().toString();
+    const respuesta = await this.fireStorage.storage.ref(picName).putString(dataFoto, 'base64', { contentType: 'image/jpeg' }).
+    then((uploadFoto) => {
+      return uploadFoto.ref.getDownloadURL().then(downloadLink => {
+        return downloadLink.toString();
+      });
+    });
+
+    return respuesta;
+  }
 }
