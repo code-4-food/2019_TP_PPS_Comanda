7a322ac0f5072ef9125e4c3758ab6b8a7c64aef6
SkieeL <Skiel_Mafu-cs@hotmail.com>
2019-06-05 14:26:32 -0300

    Se establece home como ruta default

--- CodeForFood/src/app/app-routing.module.ts
+++ CodeForFood/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'alta-supervisor',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
