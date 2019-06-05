0f9e21ff68b9354ddcbd1d704dbec1a689ff3106
SkieeL <Skiel_Mafu-cs@hotmail.com>
2019-06-05 14:02:10 -0300

    Se agrega un metodo en el auth service para cargar en el localStorage los datos de un cliente anonimo, se modifica el CrearUsuario devolviendo el objeto usuario previamente seteandole el id del documento dentro del objeto

--- CodeForFood/src/app/servicios/auth.service.ts
+++ CodeForFood/src/app/servicios/auth.service.ts
@@ -19,6 +19,11 @@ export class AuthService {
     private fireStorage: AngularFireStorage,
     ) { }
 
+  setAnonimoInLocalStorage(usuario: any) {
+    localStorage.setItem('usuario', JSON.stringify(usuario));
+    alert('Usuario seteado en LS!');
+  }
+
   LogIn(mail, pass) {
     return new Promise((resolve, rejected) => {
       this.AFauth.auth.signInWithEmailAndPassword(mail, pass).then(usuarioLogeado => {
@@ -42,11 +47,6 @@ export class AuthService {
                   break;
                 case 'cliente':
                   this.usuario = obj_element as Cliente;
-                  localStorage.setItem('usuario', JSON.stringify(this.usuario))
-                  resolve(this.usuario)
-                  break;
-                case 'anonimo':
-                  this.usuario = obj_element as Anonimo;
                   localStorage.setItem('usuario', JSON.stringify(this.usuario))
                   resolve(this.usuario)
                   break;
@@ -105,21 +105,17 @@ export class AuthService {
   public CrearUsuario(usuarios, foto, identificador) {
     return new Promise((resolve, rejected) => {
       this.fireStorage.storage.ref(identificador).putString(foto, 'base64', { contentType: 'image/jpeg' }).then(
-       async () =>{
+       async () => {
         await this.fireStorage.ref(identificador).getDownloadURL().subscribe(downloadLink => {
           usuarios.foto = downloadLink;
           this.firestore.collection('usuarios').add(usuarios).then(ret => {
-            resolve(ret)
+            usuarios.id = ret.id;
+            resolve(usuarios);
           }).catch(err => {
-            rejected(err)
-          })
+            rejected(err);
+          });
         });
-       }
-      )
-    })
-
+      });
+    });
   }
-
-
-
 }
