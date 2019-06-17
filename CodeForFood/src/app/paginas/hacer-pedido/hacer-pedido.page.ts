5f8a7c7fee56a8a255a8df5f37f7eb998d71b749
Micaela Saez <mica_saez@hotmail.com>
2019-06-17 00:11:32 -0300

    modificaciones de hacer pedidos

--- CodeForFood/src/app/paginas/hacer-pedido/hacer-pedido.page.ts
+++ CodeForFood/src/app/paginas/hacer-pedido/hacer-pedido.page.ts
@@ -7,6 +7,7 @@ import { PedidosService } from 'src/app/servicios/pedidos.service';
 import { MesasService } from 'src/app/servicios/mesas.service';
 import { AuthService } from 'src/app/servicios/auth.service';
 import { Router } from '@angular/router';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-hacer-pedido',
@@ -19,40 +20,41 @@ export class HacerPedidoPage implements OnInit {
   public pedido = {
     comienzo: (new Date()).toString(),
     estado: 'sconfirmar',
-    id_mesa_cliente: ''
+    id_mesa_cliente: '',
+    'id-mesa': ''
   };
-  private pedidosProductos = [];
+  public pedidosProductos = [];
   public mesasClientes = [];
   private idUsusario = '';
   private usuario;
   public esMozo: boolean;
 
   constructor(private prodServ: ProductosService, private pedidoServ: PedidosService,
-    private mesaServ: MesasService, private authServ: AuthService, private router: Router) {
+    private mesaServ: MesasService, private authServ: AuthService,
+    private router: Router, private barcodeScanner: BarcodeScanner) {
     this.prodServ.getProductos().subscribe( (data) => {
       this.productos = data;
       console.log(data);
     });
     this.cantidad = 1;
+  }
+  ngOnInit() {
     this.idUsusario = this.authServ.getUsuario()['id'];
     this.usuario = this.authServ.getUsuario();
     this.mesaServ.getMesasClientes().subscribe( (data) => {
       this.mesasClientes = data;
-      if (this.usuario.perfil != 'cliente') {
+      if (this.usuario.perfil != 'cliente' && this.usuario.perfil != 'anonimo') {
         this.esMozo = true;
       } else {
         this.esMozo = false;
         for (const item of this.mesasClientes) {
           if (item.idCliente === this.idUsusario) {
             this.pedido.id_mesa_cliente = item.id;
-            // console.log('encontro id mesa cliente', this.pedido);
             break;
           }
         }
       }
     });
-  }
-  ngOnInit() {
   }
 
   public Agregar(idProd: string) {
@@ -63,15 +65,23 @@ export class HacerPedidoPage implements OnInit {
           id_pedido: this.pedido['id'],
           estado: 'sconfirmar',
           id_producto: idProd,
-          id_comanda: ''
+          id_comanda: '',
+          cantidad: this.cantidad
         };
         this.pedidosProductos.push(pedidoProd);
       }
     }
+    this.cantidad = 1;
   }
   public TerminarPedido() {
     if (this.pedidosProductos.length > 0 ) {
-      if (this.usuario.perfil == 'cliente' && this.pedido.id_mesa_cliente != '') {
+      console.log(this.pedido.id_mesa_cliente);
+      if (this.pedido.id_mesa_cliente != '') {
+        this.mesasClientes.forEach(mCliente => {
+          if (mCliente.id == this.pedido.id_mesa_cliente) {
+            this.pedido['id-mesa'] = mCliente.idMesa;
+          }
+        });
         this.pedidoServ.AddPedido(this.pedido).then( (res) => {
           this.pedido['id'] = res['id'];
           console.log(this.pedido);
@@ -82,7 +92,7 @@ export class HacerPedidoPage implements OnInit {
             });
           }
           alert('pedido agregado!');
-          if ( this.usuario.perfil == 'cliente' ) {
+          if ( this.usuario.perfil == 'cliente' || this.usuario.perfil == 'anonimo') {
             this.router.navigate(['/home-cliente']);
           } else {
             this.router.navigate(['/home-mozo']);
@@ -93,7 +103,23 @@ export class HacerPedidoPage implements OnInit {
       }
     }
   }
+  public BorrarProducto(idProducto: string) {
+    console.log(this.pedidosProductos);
+    for (const pp of this.pedidosProductos) {
+      if (pp.id_producto == idProducto) {
+        const index = this.pedidosProductos.indexOf(pp);
+        this.pedidosProductos.splice(index, 1);
+        break;
+      }
+    }
+  }
   public LeerQR() {
-    
+    this.barcodeScanner.scan().then(resultado => {
+      this.productos.forEach(producto => {
+        if (producto.id == resultado.text) {
+          this.Agregar(producto.id);
+        }
+      });
+    });
   }
 }
