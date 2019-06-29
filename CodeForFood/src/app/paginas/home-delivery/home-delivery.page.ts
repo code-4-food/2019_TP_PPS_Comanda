bf01dca3c277b19d4d8e1f4831e654fb1ad2f3de
marcos514 <marmarreyer@gmail.com>
2019-06-28 23:47:50 -0300

    Terminar delivery--> rebase staging

--- CodeForFood/src/app/paginas/home-delivery/home-delivery.page.ts
+++ CodeForFood/src/app/paginas/home-delivery/home-delivery.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-delivery',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeDeliveryPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  Mover(lugar){
+    this.router.navigate([lugar]);
   }
 
 }
